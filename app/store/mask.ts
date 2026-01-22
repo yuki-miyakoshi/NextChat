091275f88f1c7404df74d9baaf22795677adacb7
MIYAKOSHI Yuki <miyakoshi.yuki@raz.jp>
2026-01-22 18:35:28 +0900

    Update mask.ts

--- app/store/mask.ts
+++ app/store/mask.ts
@@ -43,7 +43,7 @@ export const createEmptyMask = () =>
     lang: getLang(),
     builtin: false,
     createdAt: Date.now(),
-    plugin: [],
+    plugin: ["duckduckgolite"],
   }) as Mask;
 
 export const useMaskStore = createPersistStore(
