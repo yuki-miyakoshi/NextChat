a877c11a1d4b9649fcc2bdf12fb2e88faf11757e
MIYAKOSHI Yuki <miyakoshi.yuki@raz.jp>
2026-01-22 17:29:45 +0900

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -337,7 +337,7 @@ export function SideBar(props: { className?: string }) {
               </Link>
             </div>
             <div className={styles["sidebar-action"]}>
-              <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
+              <a href={'https://github.com/yuki-miyakoshi/NextChat'} target="_blank" rel="noopener noreferrer">
                 <IconButton
                   aria={Locale.Export.MessageFromChatGPT}
                   icon={<GithubIcon />}
